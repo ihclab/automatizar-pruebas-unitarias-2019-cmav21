a50f310ab7f514dd107bd8a691894d927e411570
dcl17n15 <dcl17n15@iMac-de-administrador-2.local>
2019-03-19 10:03:51 -0600

    problema 03

--- readFile.js
+++ readFile.js
@@ -2,7 +2,21 @@ const fs = require('fs');
 
 fs.readFile('./CasosPrueba.txt', 'utf-8', (err, data)=> {
     if(err)
-        console.log(err);
-    console.log(data);
+        console.log(err); 
+    let lineas = data.split('\r\n');
+    let campos = separarCampos(lineas);
+    console.log(campos);
 });
+
+let separarCampos = (elementos) => {
+    let campos = [];
+    elementos.forEach(element => {
+        let valoresCampos = element.split(':');
+        campos.push(valoresCampos);
+    })
+    return campos;
+}
+
+
+
 
