224be097751691364d11a3ece3e9de63b6bb51c2
dcl17n15 <dcl17n15@iMac-de-administrador-2.local>
2019-03-19 10:37:16 -0600

    cambio de metodos a clases

--- Medias.js
+++ Medias.js
@@ -1,34 +1,41 @@
-let mediaAritmetica = function(vals){
-    let result = 0;
-    vals.forEach(element => {
-        result += element;
-    });
-    return (result/vals.length);
-};
 
-let raizEnesima =  function(x, n){
-    return Math.pow(x, (1/n));
-};
+class Medias {
 
-let mediaGeometrica = function(vals){
-    let result = 1;
-    vals.forEach(element => {
-        result *= element;
-    });
-    return raizEnesima(result, vals.length);
-};
+    mediaAritmetica (vals){
+        let result = 0;
+        vals.forEach(element => {
+            result += element;
+        });
+        return (result/vals.length);
+    }
 
-let mediaArmonica = function(vals){
-    let result = 0;
-    vals.forEach(element => {
-        result += (1/element);
-    })
-    result = (vals.length/result);
-    return result;
+    raizEnesima (x, n){
+        return Math.pow(x, (1/n));
+    };
+
+    mediaGeometrica (vals){
+        let result = 1;
+        vals.forEach(element => {
+            result *= element;
+        });
+        return this.raizEnesima(result, vals.length);
+    };
+
+    mediaArmonica (vals){
+        let result = 0;
+        vals.forEach(element => {
+            result += (1/element);
+        });
+        result = (vals.length/result);
+        return result;
+    }
 }
 
 let elementos = [2,4,12,6,18];
-console.log(raizEnesima(2,5));
-console.log(mediaGeometrica(elementos));
-console.log(mediaAritmetica(elementos));
-console.log(mediaArmonica(elementos));
+
+let medias = new Medias();
+
+console.log(medias.raizEnesima(2,5));
+console.log(medias.mediaGeometrica(elementos));
+console.log(medias.mediaAritmetica(elementos));
+console.log(medias.mediaArmonica(elementos));
--- readFile.js
+++ readFile.js
@@ -1,20 +1,28 @@
 const fs = require('fs');
 
-fs.readFile('./CasosPrueba.txt', 'utf-8', (err, data)=> {
-    if(err)
-        console.log(err); 
-    let lineas = data.split('\r\n');
-    let campos = separarCampos(lineas);
-    console.log(campos);
-});
+class ReadFile {
+    constructor(fs) {
+        this.fs = fs;
+    }
 
-let separarCampos = (elementos) => {
-    let campos = [];
-    elementos.forEach(element => {
-        let valoresCampos = element.split(':');
-        campos.push(valoresCampos);
-    })
-    return campos;
+    leerArchivo = () => {
+        this.fs.readFile('./CasosPrueba.txt', 'utf-8', (err, data)=> {
+            if(err)
+                console.log(err); 
+            let lineas = data.split('\r\n');
+            let campos = separarCampos(lineas);
+            console.log(campos);
+        });
+    }
+
+    separarCampos = (elementos) => {
+        let campos = [];
+        elementos.forEach(element => {
+            let valoresCampos = element.split(':');
+            campos.push(valoresCampos);
+        })
+        return campos;
+    }
 }
 
 
