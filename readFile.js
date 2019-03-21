091865f97b865a12cdf1b7311ea026e21e68528e
cesar magallon <cesar9mv@gmail.com>
2019-03-21 08:55:09 -0600

    problema 04

--- readFile.js
+++ readFile.js
@@ -1,21 +1,25 @@
 const fs = require('fs');
 
 class ReadFile {
-    constructor(fs) {
-        this.fs = fs;
-    }
 
-    leerArchivo = () => {
-        this.fs.readFile('./CasosPrueba.txt', 'utf-8', (err, data)=> {
+    leerArchivo () {
+        fs.readFile('./CasosPrueba.txt', 'utf-8', (err, data)=> {
             if(err)
                 console.log(err); 
             let lineas = data.split('\r\n');
-            let campos = separarCampos(lineas);
-            console.log(campos);
+            let campos = this.separarCampos(lineas);
+            let valores = [];
+            campos.forEach(vector => {
+                if(vector[2] === 'NULL')
+                    valores.push(null);
+                else 
+                    vector[2] === 'NULL' ? vector.push(null) : valores.push(this.convertirCadena(vector[2])) 
+;
+            });
         });
     }
 
-    separarCampos = (elementos) => {
+    separarCampos (elementos) {
         let campos = [];
         elementos.forEach(element => {
             let valoresCampos = element.split(':');
@@ -23,7 +27,19 @@ class ReadFile {
         })
         return campos;
     }
+    
+    convertirCadena(cadena){
+        let elementos = cadena.split(' ');
+        let valores = [];
+        elementos.forEach(elemento => {
+            valores.push(parseInt(elemento,10));
+        })
+        return valores;
+    }
 }
+
+let readFile = new ReadFile();
+readFile.leerArchivo();
 
 
 
