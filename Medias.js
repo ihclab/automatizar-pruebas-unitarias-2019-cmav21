fefc130f517c5913ad10e61646eb2f5dfb754df7
cesar magallon <cesar9mv@gmail.com>
2019-03-21 16:10:09 -0600

    problema 05

--- Medias.js
+++ Medias.js
@@ -16,26 +16,28 @@ class Medias {
     mediaGeometrica (vals){
         let result = 1;
         vals.forEach(element => {
-            result *= element;
         });
         return this.raizEnesima(result, vals.length);
     };
 
     mediaArmonica (vals){
-        let result = 0;
-        vals.forEach(element => {
-            result += (1/element);
-        });
-        result = (vals.length/result);
-        return result;
+        // let result = 0;
+        // vals.forEach(element => {
+        //     result += (1/element);
+        // });
+        // result = (vals.length/result);
+        // return result;
+        return 'dude aqui no hay nada';
     }
 }
 
-let elementos = [2,4,12,6,18];
+module.exports = Medias;
 
-let medias = new Medias();
+// let elementos = [2,4,12,6,18];
 
-console.log(medias.raizEnesima(2,5));
-console.log(medias.mediaGeometrica(elementos));
-console.log(medias.mediaAritmetica(elementos));
-console.log(medias.mediaArmonica(elementos));
+// let medias = new Medias();
+
+// console.log(medias.raizEnesima(2,5));
+// console.log(medias.mediaGeometrica(elementos));
+// console.log(medias.mediaAritmetica(elementos));
+// console.log(medias.mediaArmonica(elementos));
--- readFile.js
+++ readFile.js
@@ -1,22 +1,50 @@
 const fs = require('fs');
+const claseMedia = require('./Medias');
 
 class ReadFile {
+    constructor(){
+        this.medias = new claseMedia();
+    }
 
-    leerArchivo () {
+    leerArchivo () {        
         fs.readFile('./CasosPrueba.txt', 'utf-8', (err, data)=> {
             if(err)
                 console.log(err); 
             let lineas = data.split('\r\n');
             let campos = this.separarCampos(lineas);
             let valores = [];
+            let pruebas = [];
             campos.forEach(vector => {
-                if(vector[2] === 'NULL')
-                    valores.push(null);
-                else 
-                    vector[2] === 'NULL' ? vector.push(null) : valores.push(this.convertirCadena(vector[2])) 
-;
+                if(vector[2] != 'NULL')
+                    valores.push(this.convertirCadena(vector[2]));
             });
+            
+            for (let i = 0; i < campos.length; i++) {
+                    let valor = this.ejecutarMetodo(campos[i][1], valores[i])
+                    let res = this.comprobarResultado(valor, campos[i][3]);
+                    pruebas.push(res);
+            }
+            console.log(pruebas);
         });
+    }
+    
+    ejecutarMetodo(nombreMetodo, valores){
+        if(typeof this.medias[nombreMetodo] === 'function')
+        {
+            if(valores != null) {
+                let mediaAritmetica = this.medias[nombreMetodo](valores);
+                return mediaAritmetica;
+            } 
+        }
+    }
+
+    comprobarResultado(valor, resultado){
+        if(!isNaN(resultado)){
+            resultado = parseFloat(resultado);
+            return valor === resultado;
+        } else {
+            return false;
+        }
     }
 
     separarCampos (elementos) {
